4ac3ec3ebe9e3b277aed4278174114a0a8359748
lorinh <lorinh@gmail.com>
2010-04-06 12:51:04 +0000

    added some tests

---
+++ lib/examples/cplusplus-with-c-extension.c
@@ -0,0 +1,13 @@
+/**
+ * This is a C++ file, with a .c extension. 
+ * It's meant to test sclc's C++ autodetection
+*/
+
+
+class Point {
+private:
+    int x, y;
+public:
+    int getX() { return x;}
+    int getY() { return y;}
+};
