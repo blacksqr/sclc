3aefaa49ab4cf42e697d9bdd3cda4f577d735e82
lorinh <lorinh@gmail.com>
2010-07-12 21:47:00 +0000

    Added the IsValidLanguage function, first step in checking that the mapping file specifies real languages

--- t/tlib/SclcTest.pm
+++ t/tlib/SclcTest.pm
@@ -25,4 +25,14 @@ sub test_IsCeePlusPlus_with_c_code {
     $self->assert(!Sclc::IsCeePlusPlus($code));
 }
 
+sub test_IsValidLanguage_with_real_language {
+    my $self = shift;
+    $self->assert(Sclc::IsValidLanguage("Fortran"));
+}
+
+sub test_IsValidLanguage_with_fake_language {
+    my $self = shift;
+    $self->assert(!Sclc::IsValidLanguage("klingon"));
+}
+
 1;
